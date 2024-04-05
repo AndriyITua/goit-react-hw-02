c65a32292f723af45e8be62d7df65601aa0b33a6
Andriy Zhylchuk <zhylchuk.andriy@gmail.com>
2024-04-05 22:08:20 +0300

    add style

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,19 +1,33 @@
+import css from "../Options/Options.module.css";
+
 export default function Options({ totalFeedback, onUpdate, onUpdateReset }) {
   if (totalFeedback !== 0) {
     return (
-      <div>
-        <button onClick={() => onUpdate("good")}>Good</button>
-        <button onClick={() => onUpdate("neutral")}>Neutral</button>
-        <button onClick={() => onUpdate("bad")}>Bad</button>
+      <div className={css.container}>
+        <button className={css.button} onClick={() => onUpdate("good")}>
+          Good
+        </button>
+        <button className={css.button} onClick={() => onUpdate("neutral")}>
+          Neutral
+        </button>
+        <button className={css.button} onClick={() => onUpdate("bad")}>
+          Bad
+        </button>
         <button onClick={onUpdateReset}>Reset</button>
       </div>
     );
   } else {
     return (
-      <div>
-        <button onClick={() => onUpdate("good")}>Good</button>
-        <button onClick={() => onUpdate("neutral")}>Neutral</button>
-        <button onClick={() => onUpdate("bad")}>Bad</button>
+      <div className={css.container}>
+        <button className={css.button} onClick={() => onUpdate("good")}>
+          Good
+        </button>
+        <button className={css.button} onClick={() => onUpdate("neutral")}>
+          Neutral
+        </button>
+        <button className={css.button} onClick={() => onUpdate("bad")}>
+          Bad
+        </button>
       </div>
     );
   }
